f548cf128711818c2c5f938e841f688ec280c316
Toni <Tonih617@gmail.com>
2019-11-20 14:52:13 -0500

    fixed modal-container
    Added code for modal-container and close button.

--- js/script.js
+++ js/script.js
@@ -7,14 +7,20 @@ const card =   'card';
 const gallery = document.getElementById('gallery');
 const name = document.getElementById('name');
 let employeeData = [];//setting employee data as an empty array//
+const modalContainer = document.querySelector('modal-container');
+const modalCloseBtn = document.getElementById('modal-close-btn');
 
+document.write("<h2>Welcome to the TreeHouse Employee List</h2>");
 
 fetch(url)//using the fetch method to get the data from the specified url//
 .then(response => response.json())//starting my promise using .then to return a response from specified url//
 .then(data => data.results.map(person => { //using another promise with results.map to iterate through random employees list//
   //console.log(person);
   generateImage(person);// generating the random person images//
+  generateModal(person);
   employeeData.push(person);//using the push()method to iterate through the array of random employees//
+  showModals();
+  hideModals();
 })
 )
 console.log(employeeData);
@@ -38,86 +44,108 @@ gallery.append(employeeCard);
 
 }
 
-const employeeInfo = document.createElement("div");//creating a div element to access the modal-container//
-employeeInfo.className = "modal-container";//adding the class name modal-container to the employee info as a varible to grab the div from the html//
-let empInfo = `
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-            <h3 id="name" class="modal-name cap">name</h3>
-            <p class="modal-text">email</p>
-            <p class="modal-text cap">city</p>
-            <hr>
-            <p class="modal-text">(555) 555-5555</p>
-            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-            <p class="modal-text">Birthday: 10/21/2015</p>
-        </div>`
-        employeeInfo.innerHTML = empInfo;//accessing the employee info from the html//
-        gallery.append(employeeInfo);//appending the employee info to the DOM//
-        employeeInfo.style.display = "none";//setting the overlay to none when the page is first accesssed//
+
+function generateModal(item){//creating the generateImage function//
+  const employeeInfo = document.createElement("div");//creating a div element to access the modal-container//
+  employeeInfo.className = "modal-container";//calling the "className" to card to be used//
+  let info = `
+  <div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+      <div class="modal-info-container">
+          <img class="modal-img" src="${item.picture.large}" alt="profile picture">
+          <h3 id="name" class="modal-name cap">${item.name.first} ${item.name.last}</h3>
+          <p class="modal-text">${item.email}</p>
+          <p class="modal-text cap">${item.location.city}</p>
+          <hr>
+          <p class="modal-text">${item.phone}</p>
+          <p class="modal-text">${item.location.street.number} ${item.location.street.name}, ${item.location.city}, ${item.location.state}, ${item.location.postcode}</p>
+          <p class="modal-text">${item.dob.date}</p>
+      </div>`
+
+employeeInfo.innerHTML = info;//accessing the employee info from the html//
+gallery.append(employeeInfo);//appending the gallery info from the "innerHTML"//
+employeeInfo.style.display = "none";//setting the overlay to none when the page is first accesssed//
+
+}
+
+function showModals() {
+  $('.card').each(function() {
+    $(this).click(function() {
+      $(this).next().show();
+    })
+  })
+}
+
+function hideModals() {
+  $('.modal-close-btn').click(function() {
+    $('.modal-container').hide();
+  })
+}
 
 
-  gallery.addEventListener("click", (e) =>{//adding an event listener to the gallery with a event.target to only access nescessary fields//
-  if(e.target.id !== "gallery"){// using if statement so if anything else on the page is clicked no action will occur//
 
-  var modal = document.querySelector(".modal");
-  var button = 'button';
-  var closeButton = document.querySelector(".modal-close-btn");
+  // gallery.addEventListener("click", (e) =>{//adding an event listener to the gallery with a event.target to only access nescessary fields//
+  //
+  // console.log(e.target.className)
+  // let elementClass = e.target.className;
+  //
+  // let employeeCard = e.target.textContent('div.card');
+  // if (elementClass.includes('card')) {
+  //   employeeCard.nextSibling.style.display = 'block';
+  // } else if (elementClass = 'modal-close-button') {
+  //   e.target.parentElement.parentElement.style.display = 'none';
 
-        function toggleModal() {
-            modal.classList.toggle("show-modal");
-        }
+  // function windowOnClick(event) {
+  //            if (e.target.closest("div .card")) {
+  //                toggleModal();
+  //            }
+  //        }
+  //
+  // function toggleModal() {
+  //
+  //   modalCloseBtn.addEventListener("click", toggleModal);
+  //   closeButton.addEventListener("click", toggleModal);
+  //   window.addEventListener("click", windowOnClick);
+  //
+  //
+  // }
 
-        function windowOnClick(event) {
-            if (event.target === modal) {
-                toggleModal();
-            }
-        }
 
-  modal.addEventListener("click", toggleModal);
-  closeButton.addEventListener("click", toggleModal);
-  window.addEventListener("click", windowOnClick);
-  console.log(e.target.textContent);
+  // }
+
+  // var myValue = $( "#myDivId" ).val();
+  // console.log("#myDivId");
+
+
+
+  // if(e.target.id !== "gallery"){// using if statement so if anything else on the page is clicked no action will occur//
+  //
+  // var modal = document.querySelector(".modal-container");//storing modal as a varible with the document.querySelector to access it by class className//
+  // var button = 'button';
+  // var closeButton = document.querySelector(".modal-close-btn");
+  //
+  //       function toggleModal() {
+  //           modal.classList.toggle("show-modal");
+  //       }
+  //
+  //       function windowOnClick(event) {
+  //           if (e.target.closest("div .card")) {
+  //               toggleModal();
+  //           }
+  //       }
+  //
+  // modal.addEventListener("click", toggleModal);
+  // closeButton.addEventListener("click", toggleModal);
+  // window.addEventListener("click", windowOnClick);
+  // console.log(e.target.textContent);
 
   // const textContent = 'e.target.textContent';
   //    employeeData.forEach(employee => {
   //       if(string.includes(e.target.textContent)){
   //
-         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+         // }
+//
+// })
 
 
 
