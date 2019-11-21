c528d31614722c68318113060ed694b26a21fc18
Toni <Tonih617@gmail.com>
2019-11-21 14:44:57 -0500

    added more comments
    added more comments and started trying to get exceeds with the search button.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+
 const url = 'https://randomuser.me/api/?results=12';//storing link for random employees as a url varible//
 const ul = document.getElementById('employees'); // Get the list where we will place our employees//
 const search = 'search';
@@ -10,9 +11,12 @@ let employeeData = [];//setting employee data as an empty array//
 const modalContainer = document.querySelector('modal-container');
 const modalCloseBtn = document.getElementById('modal-close-btn');
 const element = 'element';
+const searchInput = document.getElementById('search-input');
+const searchSubmit = document.getElementById('search-submit');
 
-document.write("<h2>THANK YOU FOR VISITING GLOBAL NET!!</h2>");
-document.createElement('element');
+
+document.write("<h2>THANK YOU FOR VISITING GLOBAL NET!!</h2>");//dynamically adding a message to the bottom of the page//
+
 
 
 
@@ -27,7 +31,7 @@ fetch(url)//using the fetch method to get the data from the specified url//
   hideModals();//calling the hide modals function//
 })
 )
-console.log(employeeData);
+console.log(employeeData);//console.logging employee data//
 
 //*Helper Functions*//
 function generateImage(item){//creating the generateImage function//
@@ -43,8 +47,8 @@ function generateImage(item){//creating the generateImage function//
        <p class="card-text cap">${item.location.city}, ${item.location.state}</p>
    </div>
 `
-employeeCard.innerHTML = info;//appending the gallery info from the "innerHTML"//
-gallery.append(employeeCard);
+employeeCard.innerHTML = info;//getting innerHTML info from the HTML//
+gallery.append(employeeCard);//appending the info from the "innerHTML"//
 
 }
 
@@ -67,21 +71,58 @@ function generateModal(item){//creating the generateImage function//
       </div>`
 
 employeeInfo.innerHTML = info;//accessing the employee info from the html//
-gallery.append(employeeInfo);//appending the gallery info from the "innerHTML"//
+gallery.append(employeeInfo);//appending the info from the "innerHTML"//
 employeeInfo.style.display = "none";//setting the overlay to none when the page is first accesssed//
 
 }
 
-function showModals() {
-  $('.card').each(function() {
-    $(this).click(function() {
-      $(this).next().show();
+function showModals() {//calling the show modals function//
+  $('.card').each(function() {//iterating through each card//
+    $(this).click(function() {//adding a click handler to each card//
+      $(this).next().show();//returning and showing the next sibling element//
     })
   })
 }
 
-function hideModals() {
-  $('.modal-close-btn').click(function() {
-    $('.modal-container').hide();
+function hideModals() {//calling the hide modals function//
+  $('.modal-close-btn').click(function() {//adding a click handler function to the close button//
+    $('.modal-container').hide();//hiding the modal container after the close button has been activated//
   })
+
+
+
+//*Exceeds=======Search Function*//
+
+  function generateSearchInput(item){//creating the generateSearchInput function//
+    const searchContainer = document.createElement("div");//creating a div element to access the Search-container//
+    searchContainer.className = "search-input";//calling the "className" to search-input to be used//
+    let info =`
+    <form action="#" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form> -->  `
+
+  employeeSearch.innerHTML = 'search-input';//accessing the employeeSearch info from the html//
+  gallery.append(search);//appending the search//
+  // employeeSearch.style.display = "none";
+}
+function showModalBtnContainer() {//creating a show modal button container function//
+  $('.search').each(function() {//iterating through the employees to search//
+    $(this).click(function() {//adding a click handler to the search button//
+      $(this).next().show();//accessing the next employee//
+      $(this).prev().show();//accessing the previous employee//
+    })
+  })
+}
+
+function hideModals() {//creating a function to hide modal when page loads//
+  $('.modal-close-btn').click(function() {//adding a click handler to the modal close button//
+    $('.modal-container').hide();//hiding the modal after the close button has been activated//
+  })
+
+// searchInput.addEventListener('keyup', inputSearch, false);
+// searchSubmit.addEventListener('click', inputSearch, false);
+//
+
+}
 }
